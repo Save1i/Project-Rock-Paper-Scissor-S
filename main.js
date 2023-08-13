5b1b4e6ba2a31f91ad9ee07aeb2ed459a7558488
Savelii Kosevich <savva0512@gmail.com>
2023-08-13 20:21:16 +0300

    гдето ошибка.

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let comp = function random() {
 
 // alert(comp());
 
-function getComputerChoice() {
+let computerSelection = function getComputerChoice() {
     if (comp() == 0) {
         return "Камень";
     } else if (comp() == 1) {
@@ -13,5 +13,48 @@ function getComputerChoice() {
     } else {
         return "Бумага";
     }
+};
+
+function playerSelection() {
+    let playerChoiceNoRegister = prompt(
+        "Выбери: Камень, Ножницы, Бумага!",
+        "Бумага"
+    );
+
+    let playerChoice = toLowerCase(playerChoiceNoRegister);
+
+    if (playerChoice == "камень") {
+        return "Камень";
+    } else if (playerChoice == "ножницы") {
+        return "Ножницы";
+    } else if (playerChoice == "бумага") {
+        return "Бумага";
+    } else {
+        console.log("Не мухлюй");
+    }
 }
-getComputerChoice();
+
+// Ситуации:
+// 1. камень бьет ножницы
+// 2. бумага бьет камень
+// 3. ножницы бьют бумагу
+
+function playRound() {
+    if (computerSelection == "Камень" && playerSelection == "Ножницы") {
+        return "Камень бьет ножницы. Ты проигал(";
+    } else if (computerSelection == "Бумага" && playerSelection == "Камень") {
+        return "Бумага бьет камень. Ты проиграл(";
+    } else if (computerSelection == "Ножницы" && playerSelection == "Бумага") {
+        return "Ножницы бьют бумагу. Ты проиграл(";
+    } else if (computerSelection == "Бумага" && playerSelection == "Ножницы") {
+        return "Ножницы бьют бумагу. Ты выйграл)";
+    } else if (computerSelection == "Ножницы" && playerSelection == "Камень") {
+        return "Камень бьет ножницы. Ты выйграл)";
+    } else if (computerSelection == "Камень" && playerSelection == "Бумага") {
+        return "Бумага бьет камень. Ты выйграл)";
+    } else {
+        return "Ничья!";
+    }
+}
+
+playRound();
